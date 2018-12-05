f9a8ecbaf75c15320c0812c514231565e8988f2b
facundonapal <indiecitus@gmail.com>
2018-12-05 16:30:25 +0100

    hompage done, lookbook justcavalli on going

--- js/showmenu.js
+++ js/showmenu.js
@@ -6,9 +6,17 @@
 	  		menu.style.display = "inline-block";
 	  		backgroundW.style.background = "#fff";
 	  		backgroundW2.style.background = "#fff";
+
 	  	}
 
 	  	function hideMenu () {
 	  		var menu = document.getElementById('_hiddensubmenu');
+	  		var backgroundT = document.getElementById('backW-1');
+	  		var backgroundT2 = document.getElementById('backW-2');
+	  		var backgroundT3 = document.getElementById('whitebg');
 	  		menu.style.display = "none";
+	  		backgroundT.style.background = "transparent"; 
+	  		backgroundT2.style.background = "transparent"; 
+	  		backgroundT3.style.background = "transparent"; 
+
 	  	}
