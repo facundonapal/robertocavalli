36cf1f140a9942057fd92d34aa8445a7257c2f5c
facundonapal <indiecitus@gmail.com>
2018-12-05 13:33:21 +0100

    hidden/showed menu issue solved

---
+++ js/showmenu.js
@@ -0,0 +1,14 @@
+
+	  	function showMenu() {
+	  		var menu = document.getElementById('_hiddensubmenu');
+	  		var backgroundW = document.getElementById('backW-1');
+	  		var backgroundW2 = document.getElementById('backW-2');
+	  		menu.style.display = "inline-block";
+	  		backgroundW.style.background = "#fff";
+	  		backgroundW2.style.background = "#fff";
+	  	}
+
+	  	function hideMenu () {
+	  		var menu = document.getElementById('_hiddensubmenu');
+	  		menu.style.display = "none";
+	  	}
