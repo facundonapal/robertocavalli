f6dd7947d816735dd49f1b9de02abb31c4f29b8b
facundonapal <indiecitus@gmail.com>
2018-12-10 16:56:04 +0100

    two lookbooks done, PdP on going

--- js/showmenu.js
+++ js/showmenu.js
@@ -6,17 +6,19 @@
 	  		menu.style.display = "inline-block";
 	  		backgroundW.style.background = "#fff";
 	  		backgroundW2.style.background = "#fff";
+	  		backgroundW2.style.color = "#000";
 
 	  	}
 
 	  	function hideMenu () {
 	  		var menu = document.getElementById('_hiddensubmenu');
-	  		var backgroundT = document.getElementById('backW-1');
-	  		var backgroundT2 = document.getElementById('backW-2');
+	  		var backgroundW = document.getElementById('backW-1');
+	  		var backgroundW2 = document.getElementById('backW-2');
 	  		var backgroundT3 = document.getElementById('whitebg');
 	  		menu.style.display = "none";
-	  		backgroundT.style.background = "transparent"; 
-	  		backgroundT2.style.background = "transparent"; 
+	  		backgroundW.style.background = "transparent"; 
+	  		backgroundW2.style.background = "transparent"; 
+	  		backgroundW2.style.color = "#fff"; 
 	  		backgroundT3.style.background = "transparent"; 
 
 	  	}
